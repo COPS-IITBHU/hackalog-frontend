2cab96c0be6ad1e60286992e5ca04a666222cd9f
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-10-11 21:39:29 +0530

    - Removed auto generated components and files - Project set up - Added draft header and homepage

---
+++ components/Header/Header.js
@@ -0,0 +1,26 @@
+export default function Header() {
+    return(
+        <div>
+            <div className="d-flex align-items-center justify-content-between w-100 p-3">
+                <div>
+                    <div className="font-weight-bold">
+                        COPS <span className="text-monospace">Hackalog</span>
+                    </div>
+                </div>
+                <div>
+                    <div className="d-flex align-items-center">
+                        <div className="p-2">
+                            Hackathons
+                        </div>
+                        <div className="p-2">
+                            Login with Google  
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <style jsx>{`
+
+            `}</style>
+        </div>
+    )
+}
---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <meta charSet="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+                    <link rel="icon" href="" type="image/png" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
