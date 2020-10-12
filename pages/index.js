06674b37d8c16910f9a8b3cc9e1f0d4d0edc799d
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-10-12 18:39:54 +0530

    Remaining indent changes

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
           <div className="py-1 py-md-5 text-center">
             <h3>
               "That sometimes it is the very people no one images anything of, who do things no one can imagine." - Alan Turing
-						</h3>
+            </h3>
           </div>
           <div className="py-3 py-md-5">
             <div className="row no-gutters align-items-stretch justify-content-center">
@@ -21,7 +21,7 @@ export default function Home() {
                   <h5>Collaborate</h5>
                   <div>
                     To cooperate with or willingly assist an enemy of one's country and especially an occupying force
-									</div>
+                  </div>
                 </div>
               </div>
               <div className="col-12 col-md-4 p-3">
@@ -32,7 +32,7 @@ export default function Home() {
                   <h5>Learn</h5>
                   <div>
                     To cooperate with or willingly assist an enemy of one's country and especially an occupying force
-									</div>
+                  </div>
                 </div>
               </div>
               <div className="col-12 col-md-4 p-3">
@@ -43,7 +43,7 @@ export default function Home() {
                   <h5>Share</h5>
                   <div>
                     A portion belonging to, due to, or contributed by an individual or group
-									</div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -54,18 +54,18 @@ export default function Home() {
 
       </div>
       <style jsx>{`
-				.icon-container {
-					text-align: left;
-				}
-				.icon-container img {
-					margin: 20px 0px;
-					max-width: 60px;
-					border-radius: 50%;
-				}
-				.listhackathon-container {
-					background: linear-gradient(to top left, #2986a5,#0d6697,#00879a,#00776b);
-				}
-			`}</style>
+        .icon-container {
+          text-align: left;
+        }
+        .icon-container img {
+          margin: 20px 0px;
+          max-width: 60px;
+          border-radius: 50%;
+        }
+        .listhackathon-container {
+          background: linear-gradient(to top left, #2986a5,#0d6697,#00879a,#00776b);
+        }
+      `}</style>
     </div>
   )
 }
