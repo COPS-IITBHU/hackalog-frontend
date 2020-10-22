fba1d6a3c0abb7a83790362d08915cef42c4bb06
m-e-l-u-h-a-n <pururshottam.tiwari.cd.cse19@itbhu.ac.in>
2020-10-22 20:18:07 +0530

    Added HackathonCard, Date, Sorry components.
    HackathonCard: Used to display the required(minimum) information about an hackathon/event.
    Date: Used for displaying dates in a pretty way.
    Sorry: A card displaying an image and a text saying "Nothing here but check back soon".

---
+++ components/Sorry/Sorry.js
@@ -0,0 +1,12 @@
+import { Card } from "react-bootstrap";
+
+export default function Sorry() {
+  return (
+    <Card className="shadow-sm" id="main">
+      <Card.Img style={{ height: 200 }} variant="top" src="/images/no-events.jpeg" />
+      <Card.Body>
+        <Card.Text className="card-title">Nothing here for now, but check back soon.</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
