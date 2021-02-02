01305812766f204615fbb1c4d75b1771d5402e9e
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-02-02 18:51:10 +0530

    fix: url and image uploaded in public folder

--- pages/index.js
+++ pages/index.js
@@ -85,7 +85,7 @@ export default function Home() {
                 />
                 <meta
                     property="og:image"
-                    content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+                    content="https://raw.githubusercontent.com/COPS-IITBHU/hackalog-frontend/main/public/images/OG.png"
                 />
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta
@@ -99,7 +99,7 @@ export default function Home() {
                 />
                 <meta
                     property="twitter:image"
-                    content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+                    content="https://raw.githubusercontent.com/COPS-IITBHU/hackalog-frontend/main/public/images/OG.png"
                 />
                 <meta name="robots" content="index, follow" />
                 <meta
