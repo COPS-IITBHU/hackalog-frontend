87c35b1edef3f253dda8298f5d5f6eab2513c152
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-02-02 23:43:24 +0530

    fix: clip path support not prominent
    support checks enabled

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -19,9 +19,6 @@ export default function Footer() {
 
                     footer {
                         background: #fff;
-                        clip-path: polygon(100% 0, 100% 100%, 0% 100%, 0% 40%)
-                            content-box;
-                        transition: all 1s linear;
                     }
 
                     footer div {
@@ -32,8 +29,38 @@ export default function Footer() {
                     }
 
                     footer div h1 {
-                        padding-top: 50px;
+                        padding-top: 10px;
                         font-size: 18px;
+                    }
+
+                    @supports (
+                            -webkit-cclip-path: polygon(
+                                    100% 0,
+                                    100% 100%,
+                                    0% 100%,
+                                    0% 40%
+                                ) content-box
+                        )
+                        or
+                        (
+                            clip-path:
+                                polygon(100% 0, 100% 100%, 0% 100%, 0% 40%)
+                                content-box
+                        ) {
+                        footer div h1 {
+                            padding-top: 50px;
+                        }
+
+                        footer {
+                            clip-path: polygon(
+                                    100% 0,
+                                    100% 100%,
+                                    0% 100%,
+                                    0% 40%
+                                )
+                                content-box;
+                            transition: all 1s linear;
+                        }
                     }
 
                     footer div img {
@@ -47,18 +74,37 @@ export default function Footer() {
                     }
 
                     @media (min-width: 720px) {
-                        footer {
-                            clip-path: polygon(
-                                    100% 0,
-                                    100% 100%,
-                                    0% 100%,
-                                    0% 50%
-                                )
-                                content-box;
+                        footer div h1 {
+                            padding-top: 16px;
                         }
 
-                        footer div h1 {
-                            padding-top: 60px;
+                        @supports (
+                                -webkit-cclip-path: polygon(
+                                        100% 0,
+                                        100% 100%,
+                                        0% 100%,
+                                        0% 40%
+                                    ) content-box
+                            )
+                            or
+                            (
+                                clip-path:
+                                    polygon(100% 0, 100% 100%, 0% 100%, 0% 40%)
+                                    content-box
+                            ) {
+                            footer {
+                                clip-path: polygon(
+                                        100% 0,
+                                        100% 100%,
+                                        0% 100%,
+                                        0% 50%
+                                    )
+                                    content-box;
+                            }
+
+                            footer div h1 {
+                                padding-top: 60px;
+                            }
                         }
 
                         footer div p {
@@ -67,32 +113,64 @@ export default function Footer() {
                     }
 
                     @media (min-width: 1280px) {
-                        footer {
-                            clip-path: polygon(
-                                    100% 0,
-                                    100% 100%,
-                                    0% 100%,
-                                    0% 60%
-                                )
-                                content-box;
-                        }
-
                         footer div {
                             flex-direction: row-reverse;
-                            padding-top: 30px;
-                            padding-bottom: 10px;
+                            padding-top: 5px;
+                            padding-bottom: 5px;
                             justify-content: space-around;
+                        }
+
+                        footer div p {
+                            padding-bottom: 0px;
+                            font-size: 16px;
+                        }
+
+                        footer div img {
+                            width: 70px;
+                        }
+
+                        @supports (
+                                -webkit-cclip-path: polygon(
+                                        100% 0,
+                                        100% 100%,
+                                        0% 100%,
+                                        0% 40%
+                                    ) content-box
+                            )
+                            or
+                            (
+                                clip-path:
+                                    polygon(100% 0, 100% 100%, 0% 100%, 0% 40%)
+                                    content-box
+                            ) {
+                            footer {
+                                clip-path: polygon(
+                                        100% 0,
+                                        100% 100%,
+                                        0% 100%,
+                                        0% 60%
+                                    )
+                                    content-box;
+                            }
+
+                            footer div {
+                                padding-top: 30px;
+                                padding-bottom: 10px;
+                            }
+
+                            footer div p {
+                                margin-top: 60px;
+                                font-size: 16px;
+                            }
+
+                            footer div img {
+                                width: 100px;
+                            }
                         }
 
                         footer div h1 {
                             padding-top: 0px;
                             font-size: 18px;
-                        }
-
-                        footer div p {
-                            margin-top: 60px;
-                            padding-bottom: 0px;
-                            font-size: 16px;
                         }
 
                         footer div img {
