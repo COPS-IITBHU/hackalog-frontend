c04b2a3603d91ad01f0b636a7469dc2b19eb719d
sebas <simpdev7@gmail.com>
2021-05-16 19:17:09 -0400

    Fix missing dependences (#69)

--- pages/hackathon/[slug]/index.js
+++ pages/hackathon/[slug]/index.js
@@ -74,7 +74,7 @@ export default function Hackathon() {
                 })
             setLeaderboardLoading(false)
         }
-    }, [token, slug])
+    }, [token, slug, hackathon])
 
     if (error != 0) {
         return (
