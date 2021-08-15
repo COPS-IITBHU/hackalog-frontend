768a08f2679bc281340f0088bc6f60439c2092d7
Garima Singh <77723358+garimavs@users.noreply.github.com>
2021-08-15 22:14:33 +0530

    added alt text to images (#87)
    * Unused variables removed
    
    * change applied
    
    * added alternate text to images
    
    * changes applied
    
    * Delete package-lock.json
    
    Co-authored-by: Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>

--- pages/profile/[username].js
+++ pages/profile/[username].js
@@ -164,6 +164,7 @@ function Profile() {
                                         src={url}
                                         className="profile-image"
                                         shadow="4"
+                                        alt={`${userRequest.user.name}'s Profile Pic`}
                                     />
                                 </div>
                                 <div className="p-3 p-md-5">
