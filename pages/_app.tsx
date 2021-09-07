697c714bb712f607df6814eacabb72e6562a2558
Tanjiro <apoorveshukla2001@gmail.com>
2021-09-08 01:47:17 +0530

    Migrated Footer component to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ import Head from "next/head"
 
 import { AuthProvider } from "../context/auth"
 import Header from "../components/Header/Header"
-import Footer from "../components/Footer/Footer"
+import Footer from "../components/Footer/Footer.tsx"
 import { ToastContainer } from "react-toastify"
 import { AppProps } from "next/dist/next-server/lib/router/router"
 
