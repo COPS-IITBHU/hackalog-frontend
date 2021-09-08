3fc9a84dd9b0752c8eadac5af6a9badeb4673b6b
Tanjiro <apoorveshukla2001@gmail.com>
2021-09-08 16:52:19 +0530

    fixed problems related to emojis and link component

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ import Head from "next/head"
 
 import { AuthProvider } from "../context/auth"
 import Header from "../components/Header/Header"
-import Footer from "../components/Footer/Footer.tsx"
+import Footer from "../components/Footer/Footer"
 import { ToastContainer } from "react-toastify"
 
 const theme = {
