09a146c3e64d8d93e6c296181dca03d06e7ad993
Tanjiro <apoorveshukla2001@gmail.com>
2021-09-11 23:23:05 +0530

    removed import for React

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ReactElement } from "react"
+import { useState, useEffect, ReactElement } from "react"
 import { Button } from "atomize"
 import { Spinner } from "react-bootstrap"
 import { useAuth } from "../../context/auth"
