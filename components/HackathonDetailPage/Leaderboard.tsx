35551d1a52fa157217e3e034a4d96e683b59d183
Shubhanshu Saxena <shubhanshu.e01@gmail.com>
2021-10-29 11:42:11 +0530

    Refactor Hackathon Detail Page Code
    Move the Components to /Components folder in order to reduce number
    of lines in the file and easier developer experience while making
    changes.
    
    Signed-off-by: Shubhanshu Saxena <shubhanshu.e01@gmail.com>

---
+++ components/HackathonDetailPage/Leaderboard.tsx
@@ -0,0 +1,162 @@
+import Link from "next/link"
+import { PropsWithChildren } from "react"
+import { Table, Spinner, Image } from "react-bootstrap"
+import { Text } from "atomize"
+import { SubmissionsSerializer } from "../../types/backend"
+
+type LeaderboardPropTypes = PropsWithChildren<{
+    status: string
+    submissions: SubmissionsSerializer[]
+    loading: boolean
+}>
+
+export function Leaderboard({
+    status,
+    submissions,
+    loading,
+}: LeaderboardPropTypes) {
+    const submissionList = submissions
+        .sort((a, b) => b.score - a.score)
+        .map((submission, index) => (
+            <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{submission.teamName} </td>
+                <td> {submission.score}</td>
+                <td>
+                    <Link href={`/submission/${submission.id}`} passHref>
+                        <a href="/#">{submission.title}</a>
+                    </Link>
+                </td>
+            </tr>
+        ))
+    return (
+        <div>
+            <Table responsive>
+                <thead
+                    style={{
+                        backgroundColor: "rgba(0.9,0,0,0.04)",
+                    }}
+                >
+                    <tr>
+                        <th>Rank</th>
+                        <th>Team Name</th>
+                        <th>Score</th>
+                        <th>Submission Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {status == "Upcoming" ? (
+                        <tr>
+                            <td colSpan={4} className="px-3">
+                                <div
+                                    className="row rounded-bottom"
+                                    style={{
+                                        backgroundColor: "rgba(0.9,0,0,0.04)",
+                                    }}
+                                >
+                                    <div className="col-12 text-center py-3">
+                                        <Image
+                                            src="/images/rocket.svg"
+                                            style={{ height: "80px" }}
+                                            alt="Rocket Icon"
+                                        />
+                                        <p>Let the Hackathon Begin</p>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    ) : status == "Ongoing" ? (
+                        <tr>
+                            <td colSpan={4} className="px-3">
+                                <div
+                                    className="row rounded-bottom"
+                                    style={{
+                                        backgroundColor: "rgba(0.9,0,0,0.04)",
+                                    }}
+                                >
+                                    <div className="col-12 text-center py-3">
+                                        <Image
+                                            src="/images/rocket.svg"
+                                            style={{ height: "80px" }}
+                                            alt="Rocket Icon"
+                                        />
+                                        <p>
+                                            Submissions can only be viewed once
+                                            the hackathon has ended!
+                                        </p>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    ) : (
+                        <>
+                            {loading ? (
+                                <tr>
+                                    <td colSpan={4}>
+                                        <Spinner
+                                            style={{
+                                                position: "absolute",
+                                                left: "50%",
+                                            }}
+                                            animation="border"
+                                            role="status"
+                                        >
+                                            <span className="sr-only">
+                                                Loading...
+                                            </span>
+                                        </Spinner>
+                                    </td>
+                                </tr>
+                            ) : (
+                                <>
+                                    {submissionList.length ? (
+                                        <>{submissionList}</>
+                                    ) : (
+                                        <tr>
+                                            <td colSpan={4} className="px-3">
+                                                <div
+                                                    className="row rounded-bottom"
+                                                    style={{
+                                                        backgroundColor:
+                                                            "rgba(0.9,0,0,0.04)",
+                                                    }}
+                                                >
+                                                    <div className="col-12 text-center py-3">
+                                                        <Text textSize="display1">
+                                                            <span
+                                                                role="img"
+                                                                aria-label="sad face"
+                                                            >
+                                                                😔
+                                                            </span>
+                                                        </Text>
+                                                        <Text>
+                                                            No Submissions Found
+                                                        </Text>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    )}
+                                </>
+                            )}
+                        </>
+                    )}
+                </tbody>
+            </Table>
+            <style jsx>{`
+                .bg-grey {
+                    background-color: rgba(0.9, 0, 0, 0.04);
+                    display: flex;
+                    height: 7vh;
+                    margin-bottom: 4px;
+                    color: rgba(0.9, 0, 0, 0.8);
+                    padding: 10px;
+                }
+                .heading {
+                    color: black;
+                }
+            `}</style>
+        </div>
+    )
+}
---
+++ components/HackathonDetailPage/Participants.tsx
@@ -0,0 +1,152 @@
+import Link from "next/link"
+import { PropsWithChildren, useEffect, useState } from "react"
+import { Table, Spinner, Badge } from "react-bootstrap"
+import { TeamDetailSerializer } from "@/types/backend"
+import axios from "../../util/axios"
+
+type ParticipantsPropTypes = PropsWithChildren<{ slug: string }>
+
+export function Participants({ slug }: ParticipantsPropTypes) {
+    const [teams, setTeams] = useState<TeamDetailSerializer[]>([])
+    const [loading, setLoading] = useState<boolean>(true)
+
+    useEffect(() => {
+        setLoading(true)
+        if (slug) {
+            axios
+                .get<TeamDetailSerializer[]>(`/hackathons/${slug}/teams/`)
+                .then((response) => {
+                    setTeams(response.data)
+                })
+                .catch(() => {})
+                .finally(() => setLoading(false))
+        }
+    }, [slug])
+
+    return (
+        <div>
+            {loading ? (
+                <div className="text-center">
+                    <Table
+                        responsive
+                        style={{
+                            borderCollapse: "separate",
+                            borderSpacing: "0px 5px",
+                        }}
+                    >
+                        <thead
+                            style={{
+                                backgroundColor: "rgba(0.9,0,0,0.04)",
+                            }}
+                        >
+                            <tr>
+                                <th>Name</th>
+                                <th>Team Name</th>
+                                <th>Username</th>
+                            </tr>
+                        </thead>
+                    </Table>
+                    <Spinner animation="border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+                </div>
+            ) : (
+                <div>
+                    <Table
+                        responsive
+                        style={{
+                            borderCollapse: "separate",
+                            borderSpacing: "0px 5px",
+                        }}
+                    >
+                        <thead
+                            style={{
+                                backgroundColor: "rgba(0.9,0,0,0.04)",
+                            }}
+                        >
+                            <tr>
+                                <th>Name</th>
+                                <th>Team Name</th>
+                                <th>Username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {teams.map((team) => {
+                                var members = []
+                                members.push(
+                                    <tr
+                                        key={team.name + "_" + team.leader.name}
+                                        className="bg-grey rounded"
+                                    >
+                                        <td>
+                                            {team.leader.name}{" "}
+                                            <Badge variant="info">Leader</Badge>
+                                        </td>
+                                        <td>{team.name}</td>
+                                        <td>
+                                            <Link
+                                                href={`/profile/${team.leader.username}`}
+                                                passHref
+                                            >
+                                                <a href="/#">
+                                                    {team.leader.username}
+                                                </a>
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                )
+                                for (var i = 0; i < team.members.length; i++) {
+                                    if (
+                                        team.members[i].username !=
+                                        team.leader.username
+                                    ) {
+                                        members.push(
+                                            <tr
+                                                key={
+                                                    team.name +
+                                                    "_" +
+                                                    team.members[i].username
+                                                }
+                                                className="bg-grey rounded"
+                                            >
+                                                <td>{team.members[i].name}</td>
+                                                <td>{team.name}</td>
+                                                <td>
+                                                    <Link
+                                                        href={`/profile/${team.members[i].username}`}
+                                                        passHref
+                                                    >
+                                                        <a href="/#">
+                                                            {
+                                                                team.members[i]
+                                                                    .username
+                                                            }
+                                                        </a>
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        )
+                                    }
+                                }
+                                return members
+                            })}
+                        </tbody>
+                    </Table>
+                    {!teams.length ? (
+                        <div className="bg-grey text-center py-3 rounded">
+                            No Participants Found
+                        </div>
+                    ) : null}
+                    <style jsx>{`
+                        .bg-grey {
+                            margin-top: 2px;
+                            background-color: rgba(0.9, 0, 0, 0.04);
+                            height: 7vh;
+                            margin-bottom: 4px;
+                        }
+                    `}</style>
+                </div>
+            )}
+        </div>
+    )
+}
